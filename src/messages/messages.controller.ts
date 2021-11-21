978f72847fbaca0753ef4cb61063a3dc6e42c2b8
Ali Hasan Md. Linkon <linkon3.1416@gmail.com>
2021-11-21 19:09:01 +0600

    Add messages module, controller & setup http request

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+@Controller('messages')
+export class MessagesController {
+    @Get()
+    listMessages() {
+
+    }
+    @Post()
+    createMessage() {
+    }
+
+    @Get('/:id')
+    getMessage() {
+    }
+}
