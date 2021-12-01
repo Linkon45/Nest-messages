a370a7a7646759b5f6ea57d37ebb1d948ded3989
Ali Hasan Md. Linkon <linkon3.1416@gmail.com>
2021-12-01 11:37:59 +0600

    Add exception handler

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, NotFoundException } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { MessagesService } from './messages.service';
 
@@ -20,7 +20,11 @@ export class MessagesController {
     }
 
     @Get('/:id')
-    getMessage(@Param('id') id: string) {
-        return this.messagesService.findOne(id);
+    async getMessage(@Param('id') id: string) {
+        const message = await this.messagesService.findOne(id);
+        if (!message) {
+            throw new NotFoundException('Message not Found');
+        }
+        return message; 
     }
 }
